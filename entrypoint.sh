d2a6aaaa7afb801434303d2b02b94261d8bd871e
svetlanaradyuk <svetlana.radyuk@gmail.com>
2022-11-29 17:30:04 +0100

    change the filepath for archived file

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ set -e
 # Upload Results
 if [ -n "${REPORT_PRESIGNED_URL}" ]; then
   echo "=== Saving report to Object storage ==="
-  tar -C /results -cf results.tar .
+  tar -zcf results.tar ../results
   curl -X PUT -H "Content-Type: application/x-tar" -T results.tar -L "${REPORT_PRESIGNED_URL}"
 fi
